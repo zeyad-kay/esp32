9e96c03d4f89f2a94745b00a8dc253e6ca7fe0a1
zeyad <zeyad_khaled@outlook.com>
2022-02-22 17:23:03 +0200

    fix sw

--- client/sw.js
+++ client/sw.js
@@ -1,37 +1,19 @@
 var CACHE_NAME = 'my-pwa-cache-v1';
 var urlsToCache = [
-    './',
-    './assets/',
-    './images/',
-    './manifest.json',
-    './main.js'
+  './',
+  './assets/',
+  './images/',
+  './manifest.json',
+  './main.js'
 ];
 
 self.addEventListener('install', function (event) {
-    // Perform install steps
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(async function (cache) {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
-  );
+  // Perform install steps
   console.log("App Installable");
 });
 self.addEventListener('activate', event => {
-    event.waitUntil(self.clients.claim());
+  event.waitUntil(self.clients.claim());
 });
 self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request)
-          .then(function(response) {
-            // Cache hit - return response
-            if (response) {
-              return response;
-            }
-            return fetch(event.request);
-          }
-        )
-      );
   console.log("fetch");
 });
