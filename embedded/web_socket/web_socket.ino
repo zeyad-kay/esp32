eca27d58c4bd812301c2897b32a384b8500d500b
zeyad <zeyad_khaled@outlook.com>
2022-02-22 14:50:48 +0200

    refactor

--- embedded/web_socket/web_socket.ino
+++ embedded/web_socket/web_socket.ino
@@ -37,21 +37,7 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
  
 }
 
-void setup() {
-    Serial.begin(115200); 
-    for(uint8_t t = 4; t > 0; t--) {
-        Serial.printf("Pease wait for %d seconds\n", t);
-        Serial.flush();
-        delay(1000);
-    }
-    while(1)
-    {
-     int networks_number = WiFi.scanNetworks();
-    for(int counter1=0;counter1<networks_number;counter1++) 
-    {
-      Serial.println(WiFi.SSID(counter1));
-      Serial.println(WiFi.RSSI(counter1));
-    }
+void connect_wifi() {
     Serial.println("Please enter the username: ");
     while(Serial.available()==0)
     {}
@@ -67,20 +53,45 @@ void setup() {
     char str_array2[password_string.length()];
     password_string.toCharArray(str_array2, password_string.length());
     char* password = strtok(str_array2, " ");
-
     
-    WiFi.begin(username, password); 
-    int counter2=0;
-    while ( WiFi.status() != WL_CONNECTED ) {
-      counter2++;
+    WiFi.begin(username, password);
+
+    int i=0;
+    while( WiFi.status() != WL_CONNECTED ) {
+      i++;
       delay ( 500 );
-      if (counter2>20)
-      { break;}
+      if (i>20) break;
       Serial.print ( "." );
     }
-       if (counter2<20)
-      { break;}
-    }        
+       if (i<20) break;
+    }
+}
+
+void print_networks()
+{
+    int networks_number = WiFi.scanNetworks();
+    for(int i=0;i<networks_number;i++) 
+    {
+      Serial.print("ssid: ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(", strength: ");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print("\n");
+    }
+}
+
+void setup() {
+    Serial.begin(115200); 
+    for(uint8_t t = 4; t > 0; t--) {
+        Serial.printf("Pease wait for %d seconds\n", t);
+        Serial.flush();
+        delay(1000);
+    }
+    while(1)
+    {
+    print_networks();
+    connect_wifi();
+    }
     Serial.print("Local IP: "); Serial.println(WiFi.localIP());
     // server address, port and URL
     webSocket.begin("esp32-ws.herokuapp.com", 80, "/sensor","text");
@@ -115,7 +126,7 @@ void loop() {
         Serial.print(temp);
         String b = "{temperature: 123}";
         webSocket.sendTXT(tempc);
-        }  
+        }
         lastUpdate = millis();
     }
 }
